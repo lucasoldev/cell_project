1c82cfe21dc6b95b70002e26d67f443de584a586
Lucas Oliveira <lucas.oliveira.profissional@hotmail.com>
2026-04-13 22:50:30 -0300

    Creating basic list HTML for acessory models pages.

--- event_types/views.py
+++ event_types/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views.generic import ListView
 from . import models
 
@@ -6,3 +8,12 @@ class EventTypeListView(ListView):
     model = models.EventType
     template_name = 'event_types_list.html'
     context_object_name = 'event_types'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        
+        return queryset
