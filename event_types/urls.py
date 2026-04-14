d6a545f3a29229d609a5bf1a1e7bc492f57c4e96
Lucas Oliveira <lucas.oliveira.profissional@hotmail.com>
2026-04-14 15:21:35 -0300

    Creating event_type complete CRUD with frontends.

--- event_types/urls.py
+++ event_types/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('event_types/list/', views.EventTypeListView.as_view(), name='event_type_list'),
     path('event_types/create/', views.EventTypeCreateView.as_view(), name='event_type_create'),
+    path('event_types/<str:pk>/detail/', views.EventTypeDetailView.as_view(), name='event_type_detail'),
+    path('event_types/<str:pk>/update/', views.EventTypeUpdateView.as_view(), name='event_type_update'),
+    path('event_types/<str:pk>/delete/', views.EventTypeDeleteView.as_view(), name='event_type_delete'),
 ]
--- event_types/views.py
+++ event_types/views.py
@@ -1,11 +1,11 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from . import models, forms
 
 
 class EventTypeListView(ListView):
     model = models.EventType
-    template_name = 'event_types_list.html'
+    template_name = 'event_type_list.html'
     context_object_name = 'event_types'
 
     def get_queryset(self):
@@ -20,7 +20,28 @@ class EventTypeListView(ListView):
 
 class EventTypeCreateView(CreateView):
     model = models.EventType
-    template_name = 'event_types_create.html'
+    template_name = 'event_type_create.html'
     form_class = forms.EventTypeForm
     success_url = reverse_lazy('event_type_list')
     context_object_name = 'event_types_create'
+
+
+class EventTypeDetailView(DetailView):
+    model = models.EventType
+    template_name = 'event_type_detail.html'
+    context_object_name = 'event_type_detail'
+
+
+class EventTypeUpdateView(UpdateView):
+    model = models.EventType
+    template_name = 'event_type_update.html'
+    form_class = forms.EventTypeForm
+    success_url = reverse_lazy('event_type_list')
+    context_object_name = 'event_type_update'
+
+
+class EventTypeDeleteView(DeleteView):
+    model = models.EventType
+    template_name = 'event_type_delete.html'
+    success_url = reverse_lazy('event_type_list')
+    context_object_name = 'event_type_delete'
